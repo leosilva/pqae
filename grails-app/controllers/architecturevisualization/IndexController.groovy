fb7347b3ccc09451778623a01d6957d32c075e12
= <leo.moreira@me.com>
2017-04-11 14:01:07 -0300

    bloqueio da página inicial para o ambiente de produção (heroku)

--- grails-app/controllers/architecturevisualization/IndexController.groovy
+++ grails-app/controllers/architecturevisualization/IndexController.groovy
@@ -1,30 +1,16 @@
 package architecturevisualization
 
+import grails.util.Environment;
+
+
 class IndexController {
 
     def index() {
+		if (Environment.getCurrent() == Environment.PRODUCTION) {
+			render view : "/error/error", model : [errorTitle : g.message(code: "error.index.heroku.title"), errorMessage: g.message(code: "error.index.heroku.message")]
+			return
+		}
 		def ans = AnalyzedSystem.list()
-//		def ans = []
-//		def an = new AnalyzedSystem(systemName : "System A", previousVersion : "1.0", nextVersion : "2.0", analyzedSystemStatus: AnalyzedSystemStatus.COMPLETED)
-//		def an1 = new AnalyzedSystem(systemName : "System A", previousVersion : "2.0", nextVersion : "3.0", analyzedSystemStatus: AnalyzedSystemStatus.COMPLETED)
-//		def an2 = new AnalyzedSystem(systemName : "System A", previousVersion : "3.0", nextVersion : "4.0", analyzedSystemStatus: AnalyzedSystemStatus.PENDING)
-//		//def an3 = new AnalyzedSystem(systemName : "System A", previousVersion : "4.0", nextVersion : "5.0", analyzedSystemStatus: AnalyzedSystemStatus.PENDING)
-//		def an4 = new AnalyzedSystem(systemName : "System B", previousVersion : "1.0", nextVersion : "2.0", analyzedSystemStatus: AnalyzedSystemStatus.COMPLETED)
-//		def an5 = new AnalyzedSystem(systemName : "System B", previousVersion : "2.0", nextVersion : "3.0", analyzedSystemStatus: AnalyzedSystemStatus.COMPLETED)
-//		def an6 = new AnalyzedSystem(systemName : "System B", previousVersion : "3.0", nextVersion : "4.0", analyzedSystemStatus: AnalyzedSystemStatus.COMPLETED)
-//		def an7 = new AnalyzedSystem(systemName : "System B", previousVersion : "4.0", nextVersion : "5.0", analyzedSystemStatus: AnalyzedSystemStatus.ERROR)
-//		//def an8 = new AnalyzedSystem(systemName : "System B", previousVersion : "5.0", nextVersion : "6.0", analyzedSystemStatus: AnalyzedSystemStatus.COMPLETED)
-//		//def an9 = new AnalyzedSystem(systemName : "System B", previousVersion : "6.0", nextVersion : "7.0", analyzedSystemStatus: AnalyzedSystemStatus.ERROR)
-//		ans += an
-//		ans += an1
-//		ans += an2
-//		//ans += an3
-//		ans += an4
-//		ans += an5
-//		ans += an6
-//		ans += an7
-//		//ans += an8
-//		//ans += an9
 		ans = ans.groupBy { it.systemName }
 		ans.each {
 			it.value = it.value.flatten().sort { it.previousVersion }
